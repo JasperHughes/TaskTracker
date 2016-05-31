42e15c2677772031ac5c724e67c7a5a63fcc3b52
JasperHughes <hugjs001@mymail.unisa.edu.au>
2016-06-01 00:39:17 +0930

    Database and Class Setups
    I have created the class files required (1 extra that wasn't in the spec
    but was in the access file).
    
    The columns "Priority" in Tasks and "Status" in Projects are in the
    database as ints, I have done this with the plan for those values in the
    classes to be put in as Enums. It will make our lives easier later on
    (including mine with storing stuff in the database).

---
+++ Task Tracker/DAO/DBInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Tracker.DAO
+{
+    static class DBInterface
+    {
+        private static SqlDataAdapter da;
+        private static string connectionString = "Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\\TaskTracker.mdf;Integrated Security=True";
+
+        private static void Initialize()
+        {
+
+        }
+    }
+}
